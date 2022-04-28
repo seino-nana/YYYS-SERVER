fc206deab233015bd332821ac5484a9307a10d19
pugna <674408526@qq.com>
2022-04-28 15:16:28 +0800

    config未解决

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const app = require('./app/index')
+const config = require('./app/config')
 
 app.listen(3000,() => {
-    console.log('http://localhost:3000');
+    console.log(`http://localhost:3000`);
 })
